99f7a17f44846ec10d6f3b0182739b18b2d1de29
Jesal Mehta <jesaldmehta@gmail.com>
2020-10-16 14:46:34 -0400

    Added the HTML structure needed for the questions page, and added eventlistener to Start Quiz button in .js, to get to the Questions page from the Welcome page

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,7 @@
 // Define references to HTML elements
+const startQuiz = document.querySelector("#start");
+const welcomePg = document.querySelector(".welcome");
+const questionRow = document.querySelector(".question-row");
 
 
 // Create an object that holds the quiz questions, multi-choice options and the answers
@@ -11,13 +14,16 @@
 // Create functions/loops
    // For setting time interval
    // For displaying questions and options to the user
-
-
+    
 // Add event listeners
    // to the Start Quiz button when user clicks it
+startQuiz.addEventListener("click", function(){
+    welcomePg.classList.add("hide");
+    questionRow.classList.remove("hide");
+})
+
    // to the options when user clicks on it to select an option
    // to the Submit button when user enters the initials to submit
    // to the Clear-Highscores button when user clicks on it to delete all the existing highscores
    // to the Take-Quiz-again button to return back to main page to restart the quiz
 
-   
