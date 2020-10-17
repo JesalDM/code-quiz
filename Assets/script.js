d452f20f8ac6e90a922efeaa40205d28462889ca
Jesal Mehta <jesaldmehta@gmail.com>
2020-10-16 23:01:49 -0400

    Created the entire functionality of View Highscores that includes storing the scores in local storage, retrieving it, sorting it and displaying the list in a table within the UI modal when user clicks on View Highscores link/button

--- Assets/script.js
+++ Assets/script.js
@@ -13,8 +13,10 @@ const submitBtn = document.querySelector("#submit");
 const initialsInput = document.querySelector("#initials");
 const takeQuizBtn = document.querySelector("#go-back");
 const finalPg = document.querySelector(".final-pg");
+const tBody = document.querySelector("tBody");
+const clearHiScoreBtn = document.querySelector("#clear");
 
-// Create an object that holds the quiz questions, multi-choice options and the answers.
+// Object that holds the quiz questions, multi-choice options and the answers.
 /* The quiz questions have been taken from the following 2 websites:
      https://www.w3schools.com/js/js_quiz.asp
      https://data-flair.training/blogs/javascript-quiz/ */
@@ -41,8 +43,9 @@ let currentIndex = 0;
 let secondsLeft = 180;
 let score = 0;
 
-// Create functions/loops
-   // For setting time interval
+/* Functions*/
+
+// This function handles the time element by setting the time interval and clearing it based on a condition
 function setTime(){
     let timer = setInterval(function(){
          secondsLeft--;
@@ -55,13 +58,48 @@ function setTime(){
      },1000);
  }
    
-// Created a function that displays the questions and the corresponding multi-choice options to the user using a loop
+// This function will display the questions and the corresponding multi-choice options to the user using a loop
 function displayQues(currQuesObj){
     questions.textContent = currQuesObj.q;
     for (let i = 0; i < currQuesObj.opt.length; i++){
     options.children[i].textContent = currQuesObj.opt[i];
     }
 }
+
+// This function will create a row in the View Highscore modal everytime a user takes the quiz and submits the initials
+function buildScoreRow(currObj){
+    const tr = document.createElement("tr");
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
+    tBody.appendChild(tr);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    td1.innerHTML = currObj.name;
+    td2.innerHTML = currObj.score;
+}
+
+// This function retrieves the highscore list from local storage and builds the UI table within the modal for displaying it to the user
+function getHighScores(){
+    const highscoreTable = JSON.parse(localStorage.getItem("QuizHighScores"));
+    tBody.textContent="";
+    highscoreTable.forEach(function(obj){
+        buildScoreRow(obj);
+    })
+}
+
+// This function sorts the array in descending order
+function sortHighScores(highscoreArray){
+    highscoreArray.sort(function(current, next){
+        if (current.score > next.score){
+            return -1
+        } else if(current.score < next.score){
+            return 1
+        }
+        return 0
+    })
+}
+
+/*Event Listeners*/
 
 // Added event listener to the Start Quiz button when user clicks it
 startQuiz.addEventListener("click", function(){
@@ -87,7 +125,7 @@ options.addEventListener("click", function(event){
             score += 2;
             points.textContent = score;
         }
-        else{
+        else {
             // if answer does not match, display "Wrong"
            displayAns.children[1].textContent  = "Wrong!";
            // take way 10 secs from remaining time for every incorrect answer per rules
@@ -111,18 +149,21 @@ options.addEventListener("click", function(event){
     };
 })
 
-// initialized an empty array that will hold objects that will store user initials and scores needed for viewing highscores
-let highscoreTable = [];
-
 // Added event listener to submit button after user enters his/her initials
 submitBtn.addEventListener("click", function(event){
     event.preventDefault();
+    // retreiving the existing Highscore list from local storage
+    const highscoreTable = JSON.parse(localStorage.getItem("QuizHighScores")) || [];
     // create an object to be pushed into the highscoreTable array
     const currScore = {};
     currScore.name = initialsInput.value;
     currScore.score = score;
-    // storing the initials and score as an object in the highscoreTable array
+    // adding the current initials and score as an object in the highscoreTable array
     highscoreTable[highscoreTable.length] = currScore;
+    // sorting the highscoreTable list after adding the current score
+    sortHighScores(highscoreTable);
+    // storing the sorted list in the local storage
+    localStorage.setItem("QuizHighScores", JSON.stringify(highscoreTable));
     // handling the score, timer and initials input box values
     initialsInput.value = "";
     countdown.textContent = 0;
@@ -144,6 +185,11 @@ takeQuizBtn.addEventListener("click", function(){
     // hide this page
     finalPg.classList.add("hide");
 })
-//Add event listeners 
-      // to the Clear-Highscores button when user clicks on it to delete all the existing highscores
-   
+
+// Added event listener to the "Clear Highscores" button when user clicks on it to delete all the existing highscores
+clearHiScoreBtn.addEventListener("click", function(){
+    // removes the existing list from the UI modal
+    tBody.textContent="";
+    // removes the existing list from the local storage
+    localStorage.removeItem("QuizHighScores");
+})
